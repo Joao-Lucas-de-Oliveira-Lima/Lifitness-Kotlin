4c43acd9018b86d3d73917ad12b76c70ed9673c4
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2023-10-04 20:11:16 -0300

    creating LiFitnessApp.kt
    calling LiFitnessApp in MainActivity

---
+++ Lifitness/app/src/main/java/com/example/lifitness/app/LiFitnessApp.kt
@@ -0,0 +1,16 @@
+package com.example.lifitness.app
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LiFitnessApp(){
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+
+    }
+}
