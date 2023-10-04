ba2035ff7c8380410ded973672055a2bf3e5b7cc
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2023-10-04 20:44:53 -0300

    creating NormalTextComponent.kt

--- Lifitness/app/src/main/java/com/example/lifitness/app/LiFitnessApp.kt
+++ Lifitness/app/src/main/java/com/example/lifitness/app/LiFitnessApp.kt
@@ -1,15 +1,18 @@
 package com.example.lifitness.app
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun LiFitnessApp(){
     Surface(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color.White)
     ){
 
     }
