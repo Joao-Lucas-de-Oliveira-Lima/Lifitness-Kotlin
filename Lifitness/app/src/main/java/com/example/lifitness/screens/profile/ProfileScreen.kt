ae32e4463fdfa9172a7da634a69ecc4b2d9ea121
João Paulo Freitas Queiroz <Joaop3595@gmail.com>
2023-10-04 19:58:13 -0300

    feat: add profile screen

--- Lifitness/app/src/main/java/com/example/lifitness/screens/profile/ProfileScreen.kt
+++ Lifitness/app/src/main/java/com/example/lifitness/screens/profile/ProfileScreen.kt
@@ -1,6 +1,41 @@
 package com.example.lifitness.screens.profile
 
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import com.example.lifitness.common.composable.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lifitness.common.ext.spacer
 
-class ProfileScreen {
+@Composable
+fun ProfileScreen(
+    modifier: Modifier
+){
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally
+        
+    ){
+        MoreTitle()
+        ImageProfile()
+        Spacer(modifier = Modifier.spacer())
+        ImageBackground()
+        BioCard(content = "TESTE")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewProfileScreen(){
+    ProfileScreen(modifier = Modifier)
 }
