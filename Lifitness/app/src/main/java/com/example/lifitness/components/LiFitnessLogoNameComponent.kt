3170fb39e44795c588bd83885fc48b3eca2dbd9c
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2023-10-04 21:05:12 -0300

    creating LiFitnessLogoNameComponent.kt

---
+++ Lifitness/app/src/main/java/com/example/lifitness/components/LiFitnessLogoNameComponent.kt
@@ -0,0 +1,31 @@
+package com.example.lifitness.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun LiFitnessLogoNameConponent(value : String){
+    Text(
+        text = value,
+        modifier = Modifier
+            .fillMaxWidth()
+            //Altura mínima
+            .heightIn(min = 80.dp),
+        style = TextStyle(
+            fontSize = 40.sp,
+            fontStyle = FontStyle.Normal,
+            fontWeight = FontWeight.Normal
+        ),
+        color = Color.White
+    )
+}
