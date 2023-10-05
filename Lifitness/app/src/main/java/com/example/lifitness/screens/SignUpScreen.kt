cb14f4025a5135f49a35e9161ee9f2b53281622f
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2023-10-04 21:04:46 -0300

    creating SignUpScreen.kt

---
+++ Lifitness/app/src/main/java/com/example/lifitness/screens/SignUpScreen.kt
@@ -0,0 +1,21 @@
+package com.example.lifitness.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.example.lifitness.components.NormalTextComponent
+
+@Composable
+fun SignUpScreen(){
+    Box(
+        modifier = Modifier
+            .background(Color.Black)
+            .fillMaxSize()
+    ){
+
+    }
+}
