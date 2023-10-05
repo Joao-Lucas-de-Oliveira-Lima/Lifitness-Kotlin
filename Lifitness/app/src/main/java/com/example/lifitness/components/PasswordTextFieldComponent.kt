61b7cfc3456dc09a2e35c58d35d76592be822725
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2023-10-05 05:50:06 -0300

    creating PasswordTextFieldComponent.kt

--- Lifitness/app/src/main/java/com/example/lifitness/components/PasswordTextFieldComponent.kt
+++ Lifitness/app/src/main/java/com/example/lifitness/components/PasswordTextFieldComponent.kt
@@ -1,17 +1,12 @@
 package com.example.lifitness.components
 
-import android.provider.CalendarContract.Colors
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material3.CheckboxDefaults.colors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
@@ -20,26 +15,39 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.lifitness.R
-import java.time.format.TextStyle
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.runtime.remember
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TextFieldComponent(labelValue : String){
-
+fun PasswordTextFieldComponent(labelValue : String){
     var textValue = remember{
         mutableStateOf("")
+    }
+    var isPasswordVisible = remember{
+        mutableStateOf(false)
+    }
+    val visualTransformation = if (isPasswordVisible.value) {
+        VisualTransformation.None // Exibir texto como está
+    } else {
+        PasswordVisualTransformation() // Ocultar caracteres da senha
     }
     OutlinedTextField(
         modifier = Modifier
             .background(Color(35, 33, 33), RoundedCornerShape(12.dp)),
         label = {
-            Text(text = labelValue, color = Color.White)},
+            Text(text = labelValue, color = Color.White)
+        },
+        visualTransformation = visualTransformation,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
         value = textValue.value,
         onValueChange = {
             textValue.value = it
@@ -50,10 +58,7 @@ fun TextFieldComponent(labelValue : String){
             focusedLabelColor = Color.White,
             cursorColor = Color.White,
         ),
-        keyboardOptions = KeyboardOptions.Default,
-        textStyle = androidx.compose.ui.text.TextStyle(color = Color.White),
-
-
+        textStyle = TextStyle(color = Color.White),
 
     )
 }
--- Lifitness/app/src/main/java/com/example/lifitness/screens/SignUpScreen.kt
+++ Lifitness/app/src/main/java/com/example/lifitness/screens/SignUpScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +20,8 @@ import com.example.lifitness.R
 import com.example.lifitness.components.BrandLogoName
 import com.example.lifitness.components.LogoComponent
 import com.example.lifitness.components.NormalTextComponent
+import com.example.lifitness.components.PasswordTextFieldComponent
+import com.example.lifitness.components.RegistrationScreenButtonComponent
 import com.example.lifitness.components.TextFieldComponent
 
 @Composable
@@ -32,30 +36,20 @@ fun SignUpScreen(){
             modifier = Modifier
                 .align(Alignment.TopCenter)
                 .fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceEvenly
         ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .padding(top = 50.dp),
-                verticalArrangement = Arrangement.SpaceEvenly
-            ){
-                LogoComponent()
-                BrandLogoName(value = stringResource(id = R.string.brand_logo_name))
-            }
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(bottom = 100.dp),
-                verticalArrangement = Arrangement.SpaceEvenly
-            ){
-                TextFieldComponent(labelValue = stringResource(id = R.string.sign_up_username_field))
-                TextFieldComponent(labelValue = stringResource(id = R.string.sign_up_email_field))
-                TextFieldComponent(labelValue = stringResource(id = R.string.sign_up_password_field))
-                TextFieldComponent(labelValue = stringResource(id = R.string.sign_up_retype_your_password_field))
-            }
-
+            Spacer(modifier = Modifier.height(20.dp))
+            LogoComponent()
+            BrandLogoName(value = stringResource(id = R.string.brand_logo_name))
+            Spacer(modifier = Modifier.height(20.dp))
+            TextFieldComponent(labelValue = stringResource(id = R.string.sign_up_username_field))
+            Spacer(modifier = Modifier.height(10.dp))
+            TextFieldComponent(labelValue = stringResource(id = R.string.sign_up_email_field))
+            Spacer(modifier = Modifier.height(10.dp))
+            PasswordTextFieldComponent(labelValue = stringResource(id = R.string.sign_up_password_field))
+            Spacer(modifier = Modifier.height(10.dp))
+            PasswordTextFieldComponent(labelValue = stringResource(id = R.string.sign_up_retype_your_password_field))
+            Spacer(modifier = Modifier.height(80.dp))
+            RegistrationScreenButtonComponent("next")
         }
     }
 }
