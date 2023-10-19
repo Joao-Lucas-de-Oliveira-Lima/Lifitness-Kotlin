bee1767ef72c79f5ca242f9479aa05d0eed9d80c
Jp <86858044+Joaopfq@users.noreply.github.com>
2023-10-18 23:38:53 -0300

    test: add buttons tests

---
+++ Lifitness/app/src/androidTest/java/com/lifitness/ButtonsTests.kt
@@ -0,0 +1,123 @@
+package com.lifitness
+
+import DefaultButton
+import SmallButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.ui.test.assertContentDescriptionContains
+import androidx.compose.ui.test.assertContentDescriptionEquals
+import androidx.compose.ui.test.assertHeightIsEqualTo
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertWidthIsEqualTo
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.unit.dp
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.lifitness.ui.theme.ButtonColorProfile
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ButtonTests {
+
+    @get:Rule
+    val buttonTestRule = createComposeRule()
+
+    @Test
+    fun testShowDefaultButtonText() {
+        buttonTestRule.setContent {
+            DefaultButton(
+                contentTitle = "TESTE",
+                contentIcon = Icons.Default.Person,
+                onClick = { /*TODO*/ },
+                buttonColor = ButtonColorProfile
+            )
+        }
+
+        buttonTestRule.onNode(hasText("TESTE"))
+            .assertExists()
+    }
+
+    @Test
+    fun testButtonHeight() {
+        buttonTestRule.setContent {
+            DefaultButton(
+                contentTitle = "TESTE",
+                contentIcon = Icons.Default.Person,
+                onClick = { /*TODO*/ },
+                buttonColor = ButtonColorProfile
+            )
+        }
+
+        buttonTestRule.onNode(hasText("TESTE"))
+            .assertHeightIsEqualTo(85.dp)
+    }
+
+    @Test
+    fun testButtonIsDisplayed() {
+        buttonTestRule.setContent {
+            DefaultButton(
+                contentTitle = "TESTE",
+                contentIcon = Icons.Default.Person,
+                onClick = { /*TODO*/ },
+                buttonColor = ButtonColorProfile
+            )
+        }
+
+        buttonTestRule.onNode(hasText("TESTE"))
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun testButtonIsEnable() {
+        buttonTestRule.setContent {
+            DefaultButton(
+                contentTitle = "TESTE",
+                contentIcon = Icons.Default.Person,
+                onClick = { /*TODO*/ },
+                buttonColor = ButtonColorProfile
+            )
+        }
+
+        buttonTestRule.onNode(hasText("TESTE"))
+            .assertIsEnabled()
+    }
+
+    @Test
+    fun testSmallButtonIsEnable() {
+        buttonTestRule.setContent {
+            SmallButton(
+                contentTitle = "TESTE",
+                onClick = { /*TODO*/ },
+                buttonColor = ButtonColorProfile
+            )
+        }
+
+        buttonTestRule.onNode(hasText("TESTE"))
+            .assertIsEnabled()
+    }
+
+    @Test
+    fun testSmallButtonHeight() {
+        buttonTestRule.setContent {
+            SmallButton(
+                contentTitle = "TESTE",
+                onClick = { /*TODO*/ },
+                buttonColor = ButtonColorProfile
+            )
+        }
+
+        buttonTestRule.onNode(hasText("TESTE"))
+            .assertHeightIsEqualTo(60.dp)
+    }
+}
