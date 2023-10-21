19f91b5e3a5f77e9f27ddc8a6527f943e78fc8ed
Matheus Leandro <matheusleandro33@gmail.com>
2023-10-21 19:40:26 -0300

    feat: add a mocked image to be used in the diet composable

---
+++ Lifitness/app/src/main/java/com/lifitness/common/composable/DietRecomendationComposable.kt
@@ -0,0 +1,58 @@
+package com.lifitness.common.composable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.lifitness.ui.theme.CardBackground
+
+@Composable
+fun DietRecomendationCard(exerciseName: String, dietCal: String) {
+    EditorDietRecomendationCard(exerciseName, dietCal)
+}
+
+@Composable
+fun EditorDietRecomendationCard(healthInstructorName: String, dietCal: String) {
+    Card(
+        modifier = Modifier
+            .width(130.dp)
+            .height(140.dp)
+            .padding(5.dp)
+            .clip(RoundedCornerShape(15)),
+        colors = CardDefaults.cardColors(
+            containerColor = CardBackground
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ImageDiet()
+            SpecificDietName(healthInstructorName)
+            SpecificDietCal(dietCal)
+        }
+    }
+}
+
+
+
+@Preview
+@Composable
+fun PreviewDietRecomendationCard() {
+    DietRecomendationCard(exerciseName = "Cassandra", "200CAL")
+}
