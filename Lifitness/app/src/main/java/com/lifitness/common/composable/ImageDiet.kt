6c091cf1aa6e1fc77b92ad702e5f2a2e123dd1ed
Matheus Leandro <matheusleandro33@gmail.com>
2023-10-27 12:01:33 -0300

    feat: add the nex text and images to the personal diets

--- Lifitness/app/src/main/java/com/lifitness/common/composable/ImageDiet.kt
+++ Lifitness/app/src/main/java/com/lifitness/common/composable/ImageDiet.kt
@@ -1,11 +1,15 @@
 package com.lifitness.common.composable
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -26,9 +30,28 @@ fun ImageDiet() {
     )
 }
 
+@Composable
+fun ImagePersonalDiet() {
+        Image(
+            painter = painterResource(R.drawable.healthy_diet),
+            contentScale = ContentScale.FillBounds,
+            contentDescription = "Exercise picture",
+            modifier = Modifier
+                .fillMaxSize()
+                .blur(10.dp),
+
+        )
+}
+
 
 @Preview
 @Composable
 fun PreviewIImageDiet() {
     ImageDiet()
+}
+
+@Preview
+@Composable
+fun PreviewIImagePersonalDiet() {
+    ImagePersonalDiet()
 }
--- Lifitness/app/src/main/java/com/lifitness/common/composable/TextComposable.kt
+++ Lifitness/app/src/main/java/com/lifitness/common/composable/TextComposable.kt
@@ -1,5 +1,9 @@
 package com.lifitness.common.composable
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,6 +12,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.lifitness.R.string as AppText
@@ -158,5 +163,29 @@ fun DietsTitle(){
         fontSize = 24.sp,
         modifier = Modifier.padding(start = 20.dp).padding(top = 20.dp),
         fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun PersonalNutricionistTitle(){
+    Text(stringResource(AppText.personalDiet_title),
+        color = TextColor,
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 20.sp,
+        modifier = Modifier.padding(start = 20.dp).padding(top = 20.dp),
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun ContratPersonalDietTitle(){
+    Text(stringResource(AppText.hirePersonalDiet_title),
+        color = Color.Black,
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 20.sp,
+        modifier = Modifier
+            .fillMaxWidth(),
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
     )
 }
