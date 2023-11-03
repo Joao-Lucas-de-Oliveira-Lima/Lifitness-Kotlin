80d282a83d7156b2b54f586d53a0d4d1e403d774
Matheus Leandro <matheusleandro33@gmail.com>
2023-11-03 20:20:55 -0300

    refactor: remove unused imports

--- Lifitness/app/src/main/java/com/lifitness/common/composable/ExerciseImageComposable.kt
+++ Lifitness/app/src/main/java/com/lifitness/common/composable/ExerciseImageComposable.kt
@@ -1,9 +1,6 @@
 package com.lifitness.common.composable
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
--- Lifitness/app/src/main/java/com/lifitness/common/composable/TextComposable.kt
+++ Lifitness/app/src/main/java/com/lifitness/common/composable/TextComposable.kt
@@ -1,7 +1,6 @@
 package com.lifitness.common.composable
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,7 +12,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.lifitness.R
