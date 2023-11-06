c9688baf4d5274b2b052f47b76648aee5a09855f
Jp <86858044+Joaopfq@users.noreply.github.com>
2023-11-06 17:16:27 -0300

    feat: create home screen

---
+++ Lifitness/app/src/main/java/com/lifitness/screens/home/HomeScreen.kt
@@ -0,0 +1,59 @@
+package com.lifitness.screens.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lifitness.common.composable.DietRecomendationCard
+import com.lifitness.common.composable.DietsRecomendationTitle
+import com.lifitness.common.composable.ExerciseCard
+import com.lifitness.common.composable.IntermediateTitle
+import com.lifitness.common.composable.Navbar
+import com.lifitness.common.ext.spacer
+import com.lifitness.ui.theme.BackgroundColor
+
+@Composable
+fun HomeScreen(){
+    LazyColumn(
+        userScrollEnabled = true,
+                modifier = Modifier
+            .background(BackgroundColor)
+            .fillMaxHeight()) {
+        item {
+            Column {
+                IntermediateTitle()
+                ExerciseCard("TREINO DE ABDÔMEN", "26 MIN")
+                ExerciseCard("TREINO DE PEITO", "15 MIN")
+                ExerciseCard("TREINO DE PEITO", "15 MIN")
+            }
+        }
+
+        item{
+            Column {
+                DietsRecomendationTitle()
+                Row {
+                    DietRecomendationCard(exerciseName = "Arroz Branco", "200CAL")
+                    DietRecomendationCard(exerciseName = "Feijão Mulato", "200CAL")
+                    DietRecomendationCard(exerciseName = "Feijão Mulato", "200CAL")
+                }
+                Row {
+                    DietRecomendationCard(exerciseName = "Arroz Branco", "200CAL")
+                    DietRecomendationCard(exerciseName = "Feijão Mulato", "200CAL")
+                    DietRecomendationCard(exerciseName = "Feijão Mulato", "200CAL")
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewHomeScreen(){
+    HomeScreen()
+}
