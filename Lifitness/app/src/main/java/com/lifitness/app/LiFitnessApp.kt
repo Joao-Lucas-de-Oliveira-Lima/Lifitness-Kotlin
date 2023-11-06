5d96b2b479088face5eaf8d9df3eb8ca49453f38
Jp <86858044+Joaopfq@users.noreply.github.com>
2023-11-06 17:16:43 -0300

    feat: create navbar

--- Lifitness/app/src/main/java/com/lifitness/app/LiFitnessApp.kt
+++ Lifitness/app/src/main/java/com/lifitness/app/LiFitnessApp.kt
@@ -2,12 +2,52 @@ package com.lifitness.app
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BarChart
+import androidx.compose.material.icons.filled.Egg
+import androidx.compose.material.icons.filled.FitnessCenter
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.BarChart
+import androidx.compose.material.icons.outlined.Egg
+import androidx.compose.material.icons.outlined.FitnessCenter
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.lifitness.screens.home.HomeScreen
 import com.lifitness.screens.login.LoginScreen
+import com.lifitness.ui.theme.CardBackground
+import com.lifitness.ui.theme.RedChart
 
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasNews: Boolean,
+)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LiFitnessApp() {
     Surface(
@@ -15,6 +55,83 @@ fun LiFitnessApp() {
             .fillMaxSize()
             .background(Color.White)
     ) {
-        LoginScreen()
+        val items = listOf(
+            BottomNavigationItem(
+                title = "Home",
+                selectedIcon = Icons.Filled.Home,
+                unselectedIcon = Icons.Outlined.Home,
+                hasNews = false,
+            ),
+            BottomNavigationItem(
+                title = "Diet",
+                selectedIcon = Icons.Filled.Egg,
+                unselectedIcon = Icons.Outlined.Egg,
+                hasNews = false,
+            ),
+            BottomNavigationItem(
+                title = "Train",
+                selectedIcon = Icons.Filled.FitnessCenter,
+                unselectedIcon = Icons.Outlined.FitnessCenter,
+                hasNews = false,
+            ),
+            BottomNavigationItem(
+                title = "Charts",
+                selectedIcon = Icons.Filled.BarChart,
+                unselectedIcon = Icons.Outlined.BarChart,
+                hasNews = false,
+            ),
+            BottomNavigationItem(
+                title = "Profile",
+                selectedIcon = Icons.Filled.Person,
+                unselectedIcon = Icons.Outlined.Person,
+                hasNews = false,
+            ),
+        )
+        var selectedItemIndex by rememberSaveable {
+            mutableStateOf(0)
+        }
+        Scaffold(
+            bottomBar = {
+                NavigationBar(
+                    containerColor = CardBackground,
+                    contentColor = RedChart
+                ) {
+                    items.forEachIndexed {index, item ->
+                            BottomNavigationItem(
+                                selected = selectedItemIndex == index,
+                                onClick = {
+                                    selectedItemIndex = index
+                                },
+                                label = {
+                                    Text(text = item.title, color = Color.White)
+                                },
+                                icon = {
+                                    BadgedBox(
+                                        badge = {
+                                            if(item.hasNews) {
+                                                Badge()
+                                            }
+                                        }
+                                    ) {
+                                        Icon(
+                                            imageVector = if (index == selectedItemIndex) {
+                                                item.selectedIcon
+                                            } else item.unselectedIcon,
+                                            contentDescription = item.title,
+                                            tint = RedChart,
+                                        )
+                                    }
+                                })
+                    }
+                }
+            },
+        ) {
+        }
     }
+}
+
+@Preview
+@Composable
+fun PreviewLiFitnessApp(){
+    LiFitnessApp()
 }
