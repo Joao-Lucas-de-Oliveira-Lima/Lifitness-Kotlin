3d66d46f73794e07989ec8bcee66d480f690df30
Matheus Leandro <matheusleandro33@gmail.com>
2023-11-15 22:49:19 -0300

    feat: add clickable modifier to the health instructor composable

--- Lifitness/app/src/main/java/com/lifitness/common/composable/HealthInstructorComposable.kt
+++ Lifitness/app/src/main/java/com/lifitness/common/composable/HealthInstructorComposable.kt
@@ -2,6 +2,7 @@ package com.lifitness.common.composable
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,36 +16,44 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.lifitness.ui.theme.CardBackground
 
 @Composable
-fun HealthInstructorCard(instructorName: String) {
-    EditorHealthInstructorCardCard(instructorName)
+fun HealthInstructorCard(instructorName: String, onClick: () -> Unit) {
+    EditorHealthInstructorCardCard(instructorName, onClick)
 }
 
 @Composable
-fun EditorHealthInstructorCardCard(healthInstructorName: String) {
-    Card(
+fun EditorHealthInstructorCardCard(healthInstructorName: String, onClick: () -> Unit) {
+    Box(
         modifier = Modifier
-            .width(130.dp)
-            .height(130.dp)
-            .padding(5.dp)
-            .clip(RoundedCornerShape(15)),
-        colors = CardDefaults.cardColors(
-            containerColor = CardBackground
-        )
+            .clickable(onClick = { onClick() })
+
     ) {
-        Column(
+        Card(
             modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .width(130.dp)
+                .height(130.dp)
+                .testTag("health_instructor_card")
+                .padding(5.dp)
+                .clip(RoundedCornerShape(15)),
+            colors = CardDefaults.cardColors(
+                containerColor = CardBackground
+            )
         ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 ImageHealthInstructor()
                 SpecificHealthInstructorName(healthInstructorName)
+            }
         }
     }
 }
@@ -54,5 +63,5 @@ fun EditorHealthInstructorCardCard(healthInstructorName: String) {
 @Preview
 @Composable
 fun PreviewHealthInstructorCardCard() {
-    HealthInstructorCard("Cassandra")
+    HealthInstructorCard("Cassandra", onClick = {})
 }
