da72c3a9470fac49302401ca1f7a6407617e273e
Matheus Leandro <matheusleandro33@gmail.com>
2023-11-15 21:37:11 -0300

    feat: add test to the exercis card composable

---
+++ Lifitness/app/src/androidTest/java/com/lifitness/composables/ExerciseCardComposable.kt
@@ -0,0 +1,60 @@
+package com.lifitness.composables
+
+import androidx.compose.ui.test.assertHeightIsAtLeast
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.unit.dp
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.lifitness.common.composable.ExerciseCard
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ExerciseCardComposable {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testShowCardExerciseTexts(){
+        composeTestRule.setContent {
+            ExerciseCard("TREINO DE PERNA", "20 MIN")
+        }
+        composeTestRule.onNodeWithText("TREINO DE PERNA").assertIsDisplayed()
+        composeTestRule.onNodeWithText("20 MIN").assertIsDisplayed()
+    }
+
+    @Test
+    fun testCardExerciseDimensions(){
+        composeTestRule.setContent {
+            ExerciseCard("TREINO DE BRAÇO", "20 MIN")
+        }
+
+        composeTestRule.onNode(hasTestTag("exercise_card"))
+            .assertHeightIsAtLeast(100.dp)
+    }
+
+    @Test
+    fun testShowCardExerciseImages(){
+        composeTestRule.setContent {
+            ExerciseCard("TREINO DE BRAÇO", "20 MIN")
+        }
+        composeTestRule.onNodeWithContentDescription("Exercise picture").assertExists()
+    }
+
+    @Test
+    fun testClicableCardExerciseImages(){
+        composeTestRule.setContent {
+            ExerciseCard("TREINO DE BRAÇO", "20 MIN")
+        }
+
+        composeTestRule.onNode(hasText("TREINO DE BRAÇO"))
+            .assertIsEnabled()
+    }
+
+}
