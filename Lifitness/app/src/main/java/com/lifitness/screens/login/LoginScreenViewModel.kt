4fc9abbe3b407a13aeca2a3bb72dc344a26f1ed1
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2023-11-20 04:27:04 -0300

    chore: adding username validation for login

--- Lifitness/app/src/main/java/com/lifitness/screens/login/LoginScreenViewModel.kt
+++ Lifitness/app/src/main/java/com/lifitness/screens/login/LoginScreenViewModel.kt
@@ -4,8 +4,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.lifitness.domain.use_case.ValidateEmail
 
-class LoginScreenViewModel:ViewModel() {
+class LoginScreenViewModel(
+
+):ViewModel() {
 
     var state by mutableStateOf(LoginFormState())
 
